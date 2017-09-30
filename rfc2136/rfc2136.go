a1b594f3f632e22a235f4738c49b2cc9443f051d
Lars Hemala <lhemala@users.noreply.github.com>
2017-09-30 17:33:38 +0200

    update rfc2136 (#12)
    * update rfc2136
    
    * make dns propagation timeout optional

--- rfc2136/rfc2136.go
+++ rfc2136/rfc2136.go
@@ -21,12 +21,20 @@ func init() {
 //         credentials[1] = TSIG algorithm
 //         credentials[2] = TSIG key
 //         credentials[3] = TSIG secret
+//         DNS propagation timeout uses default from github.com/xenolf/lego/providers/dns/rfc2136 (60s)
+// len(5): credentials[0] = nameserver
+//         credentials[1] = TSIG algorithm
+//         credentials[2] = TSIG key
+//         credentials[3] = TSIG secret
+//         credentials[4] = DNS propagation timeout
 func NewDNSProvider(credentials ...string) (caddytls.ChallengeProvider, error) {
 	switch len(credentials) {
 	case 0:
 		return rfc2136.NewDNSProvider()
 	case 4:
-		return rfc2136.NewDNSProviderCredentials(credentials[0], credentials[1], credentials[2], credentials[3])
+		return rfc2136.NewDNSProviderCredentials(credentials[0], credentials[1], credentials[2], credentials[3], "")
+	case 5:
+		return rfc2136.NewDNSProviderCredentials(credentials[0], credentials[1], credentials[2], credentials[3], credentials[4])
 	default:
 		return nil, errors.New("invalid credentials length")
 	}
