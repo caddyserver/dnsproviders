6f24aa38bf89991ef36b0f1e789b44ab01e9c5e4
Chris Hobbs <chris@rx14.co.uk>
2018-02-03 06:47:18 +0000

    Add Gandiv5 provider (#19)

---
+++ gandiv5/gandiv5.go
@@ -0,0 +1,30 @@
+// Package gandiv5 adapts the lego Gandiv5 DNS provider
+// for Caddy. Importing this package plugs it in.
+package gandiv5
+
+import (
+	"errors"
+
+	"github.com/mholt/caddy/caddytls"
+	"github.com/xenolf/lego/providers/dns/gandiv5"
+)
+
+func init() {
+	caddytls.RegisterDNSProvider("gandiv5", NewDNSProvider)
+}
+
+// NewDNSProvider returns a new Gandiv5 DNS challenge provider.
+// The credentials are interpreted as follows:
+//
+// len(0): use API key from `GANDIV5_API_KEY` env var
+// len(1): credentials[0] = API key
+func NewDNSProvider(credentials ...string) (caddytls.ChallengeProvider, error) {
+	switch len(credentials) {
+	case 0:
+		return gandiv5.NewDNSProvider()
+	case 1:
+		return gandiv5.NewDNSProviderCredentials(credentials[0])
+	default:
+		return nil, errors.New("invalid credentials length")
+	}
+}
