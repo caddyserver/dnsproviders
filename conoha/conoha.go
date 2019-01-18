a572847e544ee9bae36e05a3a07f6e27e4f1f183
Kazuki Sawada <kazuki@6715.jp>
2019-01-19 02:46:57 +0900

    Add ConoHa DNS provider (#42)

---
+++ conoha/conoha.go
@@ -0,0 +1,27 @@
+// Package conoha adapts the lego ConoHa DNS provider
+// for Caddy. Importing this package plugs it in.
+package conoha
+
+import (
+	"errors"
+
+	"github.com/mholt/caddy/caddytls"
+	"github.com/xenolf/lego/providers/dns/conoha"
+)
+
+func init() {
+	caddytls.RegisterDNSProvider("conoha", NewDNSProvider)
+}
+
+// NewDNSProvider returns a new ConoHa DNS challenge provider.
+// The credentials are detected automatically; see underlying
+// package docs for details:
+// https://godoc.org/github.com/xenolf/lego/providers/dns/conoha
+func NewDNSProvider(credentials ...string) (caddytls.ChallengeProvider, error) {
+	switch len(credentials) {
+	case 0:
+		return conoha.NewDNSProvider()
+	default:
+		return nil, errors.New("invalid credentials length")
+	}
+}
