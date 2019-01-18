acaedf6680557720c999eb51b498fdc224c1618a
Daniil Rutskiy <10889589+dstdfx@users.noreply.github.com>
2019-01-18 20:44:02 +0300

    Add Vscale DNS provider (#40)
    * Add Vscale DNS-01 solver
    
    * Add ability to set token via func param
    
    * Fix comment

---
+++ vscale/vscale.go
@@ -0,0 +1,32 @@
+// Package vscale adapts the lego Vscale DNS
+// provider for Caddy. Importing this package plugs it in.
+package vscale
+
+import (
+	"errors"
+
+	"github.com/mholt/caddy/caddytls"
+	"github.com/xenolf/lego/providers/dns/vscale"
+)
+
+func init() {
+	caddytls.RegisterDNSProvider("vscale", NewDNSProvider)
+}
+
+// NewDNSProvider returns a new Vscale DNS challenge provider.
+// The credentials are interpreted as follows:
+//
+// len(0): use credentials from environment (https://godoc.org/github.com/xenolf/lego/providers/dns/vscale)
+// len(1): credentials[0] = Token
+func NewDNSProvider(credentials ...string) (caddytls.ChallengeProvider, error) {
+	switch len(credentials) {
+	case 0:
+		return vscale.NewDNSProvider()
+	case 1:
+		config := vscale.NewDefaultConfig()
+		config.Token = credentials[0]
+		return vscale.NewDNSProviderConfig(config)
+	default:
+		return nil, errors.New("invalid credentials length")
+	}
+}
