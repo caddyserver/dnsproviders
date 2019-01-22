6a5c3040444ae7bc88d8d7c97a656a9fbbfb0b3f
fuku <fukuzawa.makoto@fujitsu.com>
2019-01-22 11:09:04 +0900

    Add NIFCLOUD DNS provider (#37)

---
+++ nifcloud/nifcloud.go
@@ -0,0 +1,36 @@
+// Package nifcloud adapts the lego NIFCLOUD DNS
+// provider for Caddy. Importing this package plugs it in.
+package nifcloud
+
+import (
+	"errors"
+
+	"github.com/mholt/caddy/caddytls"
+	"github.com/xenolf/lego/providers/dns/nifcloud"
+)
+
+func init() {
+	caddytls.RegisterDNSProvider("nifcloud", NewDNSProvider)
+}
+
+// NewDNSProvider returns a new NIFCLOUD DNS challenge provider.
+// The credentials are interpreted as follows:
+//
+// len(0): use credentials from environment (https://godoc.org/github.com/xenolf/lego/providers/dns/nifcloud)
+// len(3): credentials[0] = Base URL
+//         credentials[1] = ACCESS KEY ID
+//         credentials[2] = SECRET ACCESS KEY
+func NewDNSProvider(credentials ...string) (caddytls.ChallengeProvider, error) {
+	switch len(credentials) {
+	case 0:
+		return nifcloud.NewDNSProvider()
+	case 3:
+		config := nifcloud.NewDefaultConfig()
+		config.BaseURL = credentials[0]
+		config.AccessKey = credentials[1]
+		config.SecretKey = credentials[2]
+		return nifcloud.NewDNSProviderConfig(config)
+	default:
+		return nil, errors.New("invalid credentials length")
+	}
+}
