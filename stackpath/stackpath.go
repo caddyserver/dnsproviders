fed8bd5cd18f3ffc96bfc24e2e3c2f244a50d285
Brent Hughes <bah2830@users.noreply.github.com>
2019-02-26 23:01:43 -0600

    Added stackpath dns (#35)
    * Added stackpath dns
    
    * Update stackpath.go
    
    * added new default config constructor
    
    * Added unit tests

---
+++ stackpath/stackpath.go
@@ -0,0 +1,34 @@
+package stackpath
+
+import (
+	"errors"
+
+	"github.com/mholt/caddy/caddytls"
+	"github.com/xenolf/lego/providers/dns/stackpath"
+)
+
+func init() {
+	caddytls.RegisterDNSProvider("stackpath", NewDNSProvider)
+}
+
+// NewDNSProvider returns a new Stackpath DNS challenge provider.
+// The credentials are interpreted as follows:
+//
+// len(0): use credentials from environment
+// len(3): credentials[0] = client id
+//         credentials[1] = client secret
+//         credentials[2] = stack id
+func NewDNSProvider(credentials ...string) (caddytls.ChallengeProvider, error) {
+	switch len(credentials) {
+	case 0:
+		return stackpath.NewDNSProvider()
+	case 3:
+		config := stackpath.NewDefaultConfig()
+		config.ClientID = credentials[0]
+		config.ClientSecret = credentials[1]
+		config.StackID = credentials[2]
+		return stackpath.NewDNSProviderConfig(config)
+	default:
+		return nil, errors.New("invalid credentials length")
+	}
+}
---
+++ stackpath/stackpath_test.go
@@ -0,0 +1,83 @@
+package stackpath
+
+import (
+	"errors"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/xenolf/lego/providers/dns/stackpath"
+)
+
+func TestNewDNSProvider(t *testing.T) {
+	os.Setenv("STACKPATH_CLIENT_ID", "client-id-env")
+	os.Setenv("STACKPATh_CLIENT_SECRET", "client-idsecret-env")
+	os.Setenv("STACKPATH_STACK_ID", "stack-id-env")
+
+	tests := map[string]struct {
+		credentials []string
+		want        func() (*stackpath.DNSProvider, error)
+		expectedErr error
+	}{
+		"no_creds_given": {
+			credentials: []string{},
+			want: func() (*stackpath.DNSProvider, error) {
+				return stackpath.NewDNSProvider()
+			},
+			expectedErr: nil,
+		},
+		"creds_given": {
+			credentials: []string{
+				"client-id-given",
+				"client-secret-given",
+				"stack-id-given",
+			},
+			want: func() (*stackpath.DNSProvider, error) {
+				config := stackpath.NewDefaultConfig()
+				config.ClientID = "client-id-given"
+				config.ClientSecret = "client-secret-given"
+				config.StackID = "stack-id-given"
+				return stackpath.NewDNSProviderConfig(config)
+			},
+			expectedErr: nil,
+		},
+		"bad_cred_count": {
+			credentials: []string{
+				"client-id-given",
+				"client-secret-given",
+			},
+			want: func() (*stackpath.DNSProvider, error) {
+				return nil, nil
+			},
+			expectedErr: errors.New("invalid credentials length"),
+		},
+	}
+	for ttName, tt := range tests {
+		t.Run(ttName, func(t *testing.T) {
+			expectedProvider, err := tt.want()
+			if err != nil {
+				t.Error(err)
+			}
+
+			got, err := NewDNSProvider(tt.credentials...)
+			if err == nil && tt.expectedErr != nil {
+				t.Errorf("did not get expected error: %v", tt.expectedErr)
+				return
+			}
+
+			if err != nil && tt.expectedErr == nil {
+				t.Errorf("unexpected err: %v", err)
+				return
+			}
+
+			if err != nil && err.Error() != tt.expectedErr.Error() {
+				t.Errorf("did not get error that was expected. got: %v, want: %v", err, tt.expectedErr)
+				return
+			}
+
+			if err == nil && !reflect.DeepEqual(got, expectedProvider) {
+				t.Errorf("unexpected result. got: %v, want: %v", got, expectedProvider)
+			}
+		})
+	}
+}
